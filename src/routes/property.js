760d0debad9a2fd028babf863a6e762b685334a5
Victor Ukoha <victorukay@gmail.com>
2022-05-13 02:11:06 +0100

    users can post propertie

--- src/routes/property.js
+++ src/routes/property.js
@@ -1,9 +1,11 @@
 const express=require('express')
 const router=express.Router()
 const propertyController=require('../controllers/propertyControllers')
+const verifyToken=require('../middleware/authMiddleware')
 
+const upload = require('../utils/multer')
 
-
+router.post('/',verifyToken, upload.single('image'),propertyController.create_prop)
 
 
 module.exports=router
