573c6f48c04efba421cb1317c7401dc0cf062a1b
Victor Ukoha <victorukay@gmail.com>
2022-05-13 01:00:49 +0100

    users signup feature

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -4,6 +4,6 @@ const authController=require('../controllers/authController')
 
 
 router.post('/signup',authController.sign_up)
-router.get('/signin',authController.sign_in)
+router.post('/signin',authController.sign_in)
 
 module.exports=router
